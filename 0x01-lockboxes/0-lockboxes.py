84fdcbce3a48c8afd9296bf2e772722450d14687
gomolemontlhane <gomolemontlhane@gmail.com>
2024-10-11 03:45:16 +0200

    fixed latest pep3 errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,27 @@
 This method determines if all the boxes in a given
 list of lists can be unlocked.
 
-- The function starts with the first box unlocked and uses the keys found inside
-  to unlock additional boxes.
+- The function starts with the first box unlocked
+and uses the keys found inside to unlock additional boxes.
 - Returns True if all boxes can be unlocked, otherwise returns False.
 """
+
 
 def canUnlockAll(boxes):
     n = len(boxes)  # Number of boxes
     unlocked = [False] * n  # List to track which boxes have been unlocked
     unlocked[0] = True  # The first box is always unlocked
-    
+
     to_explore = [0]  # Initialize with the first box (index 0)
-    
+
     while to_explore:
         box = to_explore.pop()  # Get the current box to explore its keys
-        
+
         # Check all keys in the current box
         for key in boxes[box]:
             if key < n and not unlocked[key]:  # If the key opens a new box
                 unlocked[key] = True  # Unlock that box
                 to_explore.append(key)  # Add the newly unlocked box
-    
+
     # If all boxes are unlocked, return True; otherwise, return False
     return all(unlocked)
