903eb4549232bffa2d4332b65b8d2b8d243e1164
gomolemontlhane <gomolemontlhane@gmail.com>
2024-11-28 20:57:41 +0200

    updated 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,17 +12,30 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
-    # Sort coins in descending order
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+
     coins.sort(reverse=True)
-    count = 0
-
-    for coin in coins:
-        if total == 0:
-            break
-        if total >= coin:
-            num_coins = total // coin
-            count += num_coins
-            total -= num_coins * coin
-
-    return count if total == 0 else -1
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
